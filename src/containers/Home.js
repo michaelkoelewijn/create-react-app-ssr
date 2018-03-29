4e63b33f33cbb2f2250964e98c4c6a1e84d1b8ca
Michael Koelewijn <michael.koelewijn@wearejust.com>
2018-03-29 14:28:13 +0200

    Add Redux for client side

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,9 +1,21 @@
 import React from 'react';
-
 import {Helmet} from "react-helmet";
 
-export default class Home extends React.Component {
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { increment } from '../actions/list';
+
+class Home extends React.Component {
+
+    handleClick() {
+        this.props.actions.increment();
+    }
+
     render() {
+
+        const { count } = this.props;
+
         return (
             <div>
                 <Helmet>
@@ -11,7 +23,23 @@ export default class Home extends React.Component {
                     <meta name="description" content="Description SEO" />
                 </Helmet>
                 <h1>Homepage</h1>
+                Clicked { count } times
+                <button onClick={this.handleClick.bind(this)}>Increment</button>
             </div>
         )
     }
 }
+
+function mapStateToProps(state) {
+    return {
+        count: state.list.count
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators({ increment }, dispatch)
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
-import {BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'react-router-redux'
+import store, { history } from './store'
 
 import App from './containers/App';
 
-render((
-    <Router>
-        <App />
-    </Router>), 
+render(
+    <Provider store={store}>
+        <div className="blabla">
+            <ConnectedRouter history={history}>
+                <App />
+            </ConnectedRouter>
+        </div>
+    </Provider>
+    , 
     document.getElementById('root')
 );
